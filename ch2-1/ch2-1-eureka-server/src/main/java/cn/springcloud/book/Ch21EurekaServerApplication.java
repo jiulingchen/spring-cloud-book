c30f8b058da371e0a668ff05b38f8742be5b590a
jiulingchen@126.com <JiulingChen90>
2018-12-03 11:52:18 +0800

    First Submit

--- ch2-1/ch2-1-eureka-server/src/main/java/cn/springcloud/book/Ch21EurekaServerApplication.java
+++ ch2-1/ch2-1-eureka-server/src/main/java/cn/springcloud/book/Ch21EurekaServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author shannon
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class Ch21EurekaServerApplication {
