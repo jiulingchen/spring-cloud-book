e28b6b56e9afe805293efee316edac5e1ba1115e
jiulingchen@126.com <JiulingChen90>
2018-12-12 13:49:16 +0800

    Spring Cloud Zuul入门案例

---
+++ ch7-2/ch7-2-client-a/src/main/java/cn/springcloud/book/ClientAApplication.java
@@ -0,0 +1,16 @@
+package cn.springcloud.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author shannon
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ClientAApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientAApplication.class,args);
+    }
+}
